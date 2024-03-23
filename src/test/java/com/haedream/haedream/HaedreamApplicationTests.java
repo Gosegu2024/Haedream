37ffce1392ff000a4a04815cdd3cb38c57cb02d7
gahyeon <als921851@icloud.com>
2024-03-23 19:14:24 +0900

    first commit

---
+++ src/test/java/com/haedream/haedream/HaedreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haedream.haedream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaedreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
