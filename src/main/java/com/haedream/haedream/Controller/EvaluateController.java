55af4eafbb5e47bdbc8f77dde3337bcec8c94885
gahyeon <als921851@icloud.com>
2024-04-03 15:26:22 +0900

    가현 - 평가하기 페이지 푸쉬를 위한 커밋 (평가하기 페이지 로그 리스트 불러오기, 리스트에 로그 아이디 정보 넣기)

--- src/main/java/com/haedream/haedream/Controller/EvaluateController.java
+++ src/main/java/com/haedream/haedream/Controller/EvaluateController.java
@@ -7,10 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.haedream.haedream.dto.request.EvalDTO;
-import com.haedream.haedream.entity.Eval;
-import com.haedream.haedream.service.EvalService;
-
 import com.haedream.haedream.entity.Log;
 import com.haedream.haedream.service.LoglistService;
 
