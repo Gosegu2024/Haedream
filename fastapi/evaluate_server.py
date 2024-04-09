4c16a5b7bb8c4d8e250943197ce6e6a49eef683b
gahyeon <als921851@icloud.com>
2024-04-09 14:41:06 +0900

    가현 - 평가결과 하이라이팅 기능 구현

--- fastapi/evaluate_server.py
+++ fastapi/evaluate_server.py
@@ -21,7 +21,8 @@ import tiktoken
 
 # 환경변수 설정
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
-
+os.environ["LANGCHAIN_API_KEY"] = ""
+os.environ["OPENAI_API_KEY"] = ""
 
 llm = ChatOpenAI(model="gpt-4-turbo-preview")
 # llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
