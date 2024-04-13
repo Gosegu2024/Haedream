0da853e7a25be4ef513dbc71e2c5f04c64083e1f
jhj0203 <jhj990203@gmail.com>
2024-04-13 15:06:52 +0900

    현지 - 해드림 패키지 0.0.9 업데이트

--- package/haedream/saveLog.py
+++ package/haedream/saveLog.py
@@ -6,18 +6,16 @@ class ModelRunner:
     server_url = "http://localhost:8088/save_data"
 
     def run_model(self, modelName, projectName, inputData, apiKey, outputData):
-        
-        # 결과를 JSON 형식으로 변환
+        warning_message = "주의 : projectName, apiKey를 잘못 입력하면 저장되지 않습니다."
+        print("\033[1m" + warning_message + "\033[0m")  # 강조하여 출력
+
         json_output = self._format_output(modelName, projectName, inputData, outputData, apiKey)
        
-        # 결과를 서버로 전송하여 저장
         self._save_to_server(json_output)
         
-        # 결과 반환
         return json_output
 
     def _format_output(self, modelName, projectName, inputData, outputData, apiKey):
-        # 결과를 JSON 형식으로 포맷팅합니다.
         result = {
             "modelName": modelName,
             "projectName": projectName,
@@ -28,7 +26,6 @@ class ModelRunner:
         return json.dumps(result)
 
     def _save_to_server(self, data):
-        # 서버로 데이터를 전송하여 저장합니다.
         
         data2 = {"js" : data}
         with requests.post(self.server_url, data=data2, stream=True) as response:
@@ -37,3 +34,7 @@ class ModelRunner:
             else:
                 print("Failed to save data to server.")
                 print(response.status_code)
+
+
+def help():
+    print("이 패키지는 해드림의 LLM output 평가를 위해 해드림 서버로 데이터를 전송할 수 있는 모듈 패키지입니다. \n사용을 위해서는 해드림 회원가입과 프로젝트 생성이 필요합니다.\n\n1. 먼저 평가할 모델의 질의를 inputData로 정의하고 결과를 outputData로 정의합니다.\n2. 회원가입 후 발급받은 apiKey를 apiKey로 정의하고 생성한 프로젝트 이름을 projectName으로 정의합니다.\n3. 모델의 이름은 modelName으로 정의합니다.\n4. runner 객체를 생성하고 run_model 함수에 각 매개변수를 채워넣고 실행합니다.\n\n  ❗ 이때 잘못된 projectName, apiKey를 입력하면 데이터가 저장되지 않으니 주의 바랍니다.")
--- package/setup.py
+++ package/setup.py
@@ -1,18 +1,18 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="haedream",
-    version="0.0.8",
+    version="0.0.9",
     author="GOSEGU",
-    author_email="",
+    author_email="jhj990203@gmail.com",
     description="This is a package for haedream",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Gosegu2024/Haedream.git", ##
+    url="https://github.com/Gosegu2024/Haedream.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
